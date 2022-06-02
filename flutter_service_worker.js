7ef86b5a7bd9d2a9ef5ba09508ea59aed9a97844
Muhammad Nur Amin <35653511+Amin05@users.noreply.github.com>
2022-06-02 14:41:11 +0700

    Add files via upload
    update commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "1dc020165a5878837899490d18c9c3aa",
+  "main.dart.js": "1c75dc17c568acc23fe77fda9f68d874",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -45,8 +45,8 @@ const RESOURCES = {
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/AssetManifest.json": "2ade6443268c481dc35899fcfb030da9",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
-"index.html": "fcb771a81ab710765e425037acf4b471",
-"/": "fcb771a81ab710765e425037acf4b471",
+"index.html": "a8dd5a0826fb70dbbce456dd0c40a70b",
+"/": "a8dd5a0826fb70dbbce456dd0c40a70b",
 "canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
 "canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
